5c45c81c9c929254a00d4744226cb8aa8e37abd2
om-benchmarkcorp <66264524+om-benchmarkcorp@users.noreply.github.com>
2020-06-10 09:55:35 -0400

    Add files via upload
    Updated main.tf and variables.tf file for the root module

--- main.tf
+++ main.tf
@@ -1,24 +1,20 @@
 provider "google"{
     version = "3.5.0"
 
-    credentials = file("sandbox-ba24fabf4dd1.json")
+    credentials = file("sandbox-2f84e3a1cbc8.json")
     project = "sandbox-279017"
     region = "us-west1"
 }
 
 module "vpc"{
     source = "./modules/vpc"
-    network_name = var.network_name
+    network_name = var.name
     auto_create_subnetworks = var.auto_create_subnetworks
     description = var.description
 }
 
 module "subnetwork"{
     source = "./modules/subnetwork"
+    subnet_name = var.name
     network_name = module.vpc.network_name
-}
-
-module "autogroup"{
-    source = "./modules/autogroup"
-    network_name = module.subnetwork.subnet_name
 }
